0148649745753c7f338e69acb1cf3ccdc777f81a
yoga212121 <yogame733@gmail.com>
2024-01-20 00:07:53 +0530

    nextjsdir

---
+++ IE_REVAMP/ie-revamp/app/page.jsx
@@ -0,0 +1,10 @@
+import Link from "next/link"
+import Homescreen from "./components/homescreen"
+export default function Home() {
+  return (
+    <main>
+      <Homescreen/>
+      
+    </main>
+  )
+}
